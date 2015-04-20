150ed661861e7af9318b683812988f946c68d58c
nekobato <nekobato@gmail.com>
2015-04-20 18:25:54 +0900

    gulpfileにless->css用の設定追加 #24

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 
 var watchify = require('watchify');
 var browserify = require('browserify');
+var path = require('path');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var assign = require('lodash.assign');
+
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 
 // Javascript
@@ -36,11 +42,27 @@ function bundle() {
 
 // Stylesheet
 
+gulp.task('css', function () {
+  return gulp.src('./src/stylesheets/**/*.less')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(concat('style.css'))
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/stylesheets'));
+});
+
 
 // Server
 
 
 
-gulp.task('default', ['js'], function() {
+// Task
 
+gulp.task('default', ['js', 'css'], function() {
+  gulp.watch('./src/stylesheets/**/*.less', ['css']);
 });
