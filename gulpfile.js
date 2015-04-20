9925012de5b943f2644f7d2eaa2a0a3cb61a1d2f
nekobato <nekobato@gmail.com>
2015-04-20 17:19:39 +0900

    gulp browserify 設定設置 #20

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+
+
+// Javascript
+
+var customOpts = {
+  entries: ['./src/javascripts/main.js'],
+  debug: true
+};
+
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+gulp.task('js', bundle);
+b.on('update', bundle);
+b.on('log', gutil.log);
+
+function bundle() {
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/javascripts'));
+}
+
+
+// Stylesheet
+
+
+// Server
+
+
+
+gulp.task('default', ['js'], function() {
+
+});
