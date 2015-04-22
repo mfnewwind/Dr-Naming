7ef0b465ebc56318d5932a0c5902841f7eb91a05
nekobato <nekobato@gmail.com>
2015-04-22 17:27:21 +0900

    本番環境用env分け追加

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var url = require('url');
 var passport = require('../lib/passports')();
 
 /* auth request */
@@ -8,13 +9,13 @@ var passport = require('../lib/passports')();
 router.get('/github',
   passport.authenticate('github'),
   function(req, res){
-    // nothing to do.
+  req.session.callback_uri = req.param('callback_uri') || '';
 });
 
 router.get('/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect(url.resolve('/', (req.session.callback_uri || '')));
 });
 
 module.exports = router;
