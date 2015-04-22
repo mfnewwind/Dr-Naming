358e0e28b94b002a40ddefe840d7b855e942436f
nekobato <nekobato@gmail.com>
2015-04-22 17:44:37 +0900

    site内で使うapiひな形

--- routes/siteapi.js
+++ routes/siteapi.js
@@ -3,4 +3,14 @@ var router = express.Router();
 
 /* サイト内で使用するAPI */
 
+router.get('/auth', ensureAuthenticated, function(req, res){
+  res.json(req.user);
+});
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.jsonp({});
+}
+
 module.exports = router;
