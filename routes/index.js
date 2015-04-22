99cdbee5ee7cfb296d249be3da916df5501692d2
nekobato <nekobato@gmail.com>
2015-04-22 16:26:10 +0900

    github login まで #7

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dr.NaminG' });
 });
+
+router.get('/login', function(req, res){
+  res.render('index', {});
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login');
+}
 
 module.exports = router;
