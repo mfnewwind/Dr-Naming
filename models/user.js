fbaf1c8c80d41c0062159b35a029547a335b01a7
nekobato <nekobato@gmail.com>
2015-04-22 15:35:37 +0900

    userModel ひな形追加 #34

--- models/user.js
+++ models/user.js
@@ -1,27 +1,11 @@
 var mongoose = require('mongoose');
 
-module.exports = function() {
+var userSchema = new mongoose.Schema({
+  id: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-  var userSchema = new mongoose.Schema({
-
-    created_at: {
-      type: Date,
-      default: Date.now
-    }
-  });
-
-  messageSchema.statics.latest = function(num, callback) {
-
-    return this.find({
-
-      })
-      .sort({
-        created_at: 'desc'
-      })
-      .limit(num)
-      .exec(callback);
-  };
-
-  mongoose.model('User', userSchema);
-
-};
+module.exports = mongoose.model('User', userSchema);
