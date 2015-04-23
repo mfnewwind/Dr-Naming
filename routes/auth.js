ff6672623b0f90bd6aff2b167659597f0e977e45
nekobato <nekobato@gmail.com>
2015-04-23 16:53:38 +0900

    github APIのpermission-scopeを変更

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,15 @@ var passport = require('../lib/passports')();
 /* auth request */
 
 router.get('/github',
-  passport.authenticate('github'),
-  function(req, res){
-  req.session.callback_uri = req.param('callback_uri') || '';
+  passport.authenticate('github', { scope: ['user:email', 'read:org'] }),
+  function(req, res) {
+    req.session.callback_uri = req.param('callback_uri') || '';
 });
 
 router.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  passport.authenticate('github', {
+    failureRedirect: '/login'
+  }),
   function(req, res) {
     res.redirect(url.resolve('/', (req.session.callback_uri || '')));
 });
