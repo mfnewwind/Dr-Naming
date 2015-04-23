e53ae66773359ed19381a8e0b71cacb7a5a3e42b
Pine Mizune <pinemz@gmail.com>
2015-04-23 11:14:41 +0900

    Repo モデル追加 (WIP)

---
+++ models/repo.js
@@ -0,0 +1,22 @@
+/*jslint node: true */
+
+var mongoose = require('mongoose');
+ 
+var fileSchema = new mongoose.Schema({
+  file_name: String,
+  path: String,
+  url: String,
+  results: []
+});
+ 
+var branchSchema = new mongoose.Schema({
+  branch_name: String,
+  files: [ fileSchema ]
+});
+ 
+var repoSchema = new mongoose.Schema({
+  repo_name: String,
+  branches: [ branchSchema ]
+});
+
+module.exports = mongoose.model('Repo', repoSchema);
