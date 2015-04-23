f03e61171908b28e1cb7c730ec34522cc749962d
Pine Mizune <pinemz@gmail.com>
2015-04-23 15:52:12 +0900

    Repo 内の  branch に対するテストを追加

--- test/models/repo-test.js
+++ test/models/repo-test.js
@@ -13,6 +13,7 @@ var Repo = require('../../models/repo');
 
 describe('### Repo models test', function () {
   var repo_name = 'github.com/mfnewwind/newwind';
+  var branch_name = 'master';
   
   describe('Repo', function () {
     it('should create', function (done) {
@@ -52,6 +53,49 @@ describe('### Repo models test', function () {
         try {
           expect(err).to.not.be.ok;
           expect(repo).to.not.be.ok;
+        }
+        
+        catch (e) { err = e; }
+        finally { done(err); }
+      });
+    });
+  });
+  
+  describe('Repo:Branch', function () {
+    it('should create with empty branch', function (done) {
+      var repo = new Repo({
+        repo_name: repo_name
+      });
+      
+      repo.branches.push({
+        branch_name: branch_name,
+        files: []
+      });
+      
+      repo.save(function (err) {
+        done(err);
+      });
+    });
+    
+    it('should find with empty branch', function (done) {
+      Repo.findOne({ repo_name: repo_name }, function (err, repo) {
+        try {
+          expect(err).to.not.be.ok;
+          expect(repo).to.have.property('_id').that.is.ok;
+          expect(repo).to.have.property('repo_name').that.equal(repo_name);
+          expect(repo.branches).to.have.length(1);
+          expect(repo.branches[0]).to.have.property('branch_name').that.equal(branch_name);
+        }
+        
+        catch (e) { err = e; }
+        finally { done(err); }
+      });
+    });
+    
+    it('should remove', function (done) {
+      Repo.remove({ repo_name: repo_name }, function (err) {
+        try {
+          expect(err).to.not.be.ok;
         }
         
         catch (e) { err = e; }
