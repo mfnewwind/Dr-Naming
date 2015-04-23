d91ba2f94cb8020e73798b960e51e3b4b31af7bb
nekobato <nekobato@gmail.com>
2015-04-23 13:02:07 +0900

    headerがjsで可変になったよ

---
+++ src/javascripts/header.js
@@ -0,0 +1,19 @@
+request = require('superagent');
+
+module.exports = {
+  template: '#header_component',
+  methods: {},
+  ready: function() {
+    var _this = this;
+    request
+      .get('/siteapi/auth')
+      .accept('json')
+      .end(function(err, res) {
+        if (err)  { return console.log('user data get error: ', err); }
+        if (!res) { return; }
+
+        _this.$root.$data.avatar = res.body._json;
+        _this.$emit('avatar-loaded');
+      });
+  }
+};
