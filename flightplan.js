1235ea02eb95069cee81805de95b2d21c9e0d0b0
Pine Mizune <pinemz@gmail.com>
2015-09-13 15:41:52 +0900

    Fix deploy script

--- flightplan.js
+++ flightplan.js
@@ -7,5 +7,27 @@ plan.target('staging', {
 });
 
 plan.remote(function (remote) {
-  remote.log('remote');
+  remote.with('cd ~/Dr-Naming', function () {
+    // git
+    remote.log('Update files');
+    remote.git('submodule init');
+    remote.git('submodule update');
+    remote.git('reset --hard HEAD');
+    remote.git('fetch origin master');
+    remote.git('checkout master');
+    remote.git('pull origin master');
+
+    // npm
+    remote.log('Install denepdencies');
+    remote.exec('npm install');
+
+    // forever
+    remote.log('Stop daemon');
+    remote.exec('forever list');
+    remote.exec('forever stop dr-naming', { failsafe: true });
+
+    remote.log('Start daemon');
+    remote.exec('forever start -a --uid dr-naming ./bin/www');
+    remote.exec('forever list');
+  });
 });
